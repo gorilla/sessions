cb4af09e63b4fa547369c43a1fa62c2c4ce5c2b7
Jonathan Gillham <jonathan.gillham@gamil.com>
2013-10-25 11:15:33 +0100

    Updated store documentation.

--- store.go
+++ store.go
@@ -16,6 +16,15 @@ import (
 )
 
 // Store is an interface for custom session stores.
+//
+// Get should return a cached session.
+// New should create and return a new session.
+// Save should persist session to the underlying store implementation.
+// 
+// Note that New should never return a nul session, even in the case of an error
+// if using the Registry infrastructure for caching of sessions in your store.
+//
+// See CookieStore and FilesystemStore for examples.
 type Store interface {
 	Get(r *http.Request, name string) (*Session, error)
 	New(r *http.Request, name string) (*Session, error)
