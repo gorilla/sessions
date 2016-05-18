7471c40f75e9566f36d1827ec7c45da026723990
Justin Hellings <justin.hellings@gmail.com>
2016-05-18 10:41:18 +0100

    Clarified use of gob.Register()
    Proposing docs change after posting this support request:
    https://groups.google.com/forum/#!topic/gorilla-web/cPcbiqFC3GU

--- doc.go
+++ doc.go
@@ -118,11 +118,10 @@ so it is easy to register new datatypes for storage in sessions:
 	}
 
 As it's not possible to pass a raw type as a parameter to a function, gob.Register()
-relies on us passing it a value, or (if we're working with pointer types) a pointer
-to a value, of the type that we want to register. In the example above we've passed
+relies on us passing it a value of the desired type. In the example above we've passed
 it a pointer to a struct and a pointer to a custom type representing a
-map[string]interface. This will then allow us to serialise/deserialise values of
-those types to and from our sessions.
+map[string]interface. (We could have passed non-pointer types if we wished.) This will
+then allow us to serialise/deserialise values of those types to and from our sessions.
 
 Note that because session values are stored in a map[string]interface{}, there's
 a need to type-assert data when retrieving it. We'll use the Person struct we registered above:
